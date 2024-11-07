7abc392e10c245a7419f61ee139222fa515d9996
Jiaqi Lv <lvjiaqi9@gmail.com>
2024-11-07 16:36:19 +0000

    update model to accept 4-channel

--- monkey/model/classification_model/efficientnet_b0.py
+++ monkey/model/classification_model/efficientnet_b0.py
@@ -1,16 +1,32 @@
 import timm
 import torch.nn as nn
 from torchvision.ops import MLP
-
+import torch
 
 class EfficientNet_B0(nn.Module):
-    def __init__(self, num_classes=2, pretrained=True):
+    def __init__(self, input_channels=3, num_classes=2, pretrained=True):
         super(EfficientNet_B0, self).__init__()
-        self.model = timm.create_model(
+        self.feature_extractor = timm.create_model(
             "efficientnet_b0",
             pretrained=pretrained,
             num_classes=0,
         )
+
+        if input_channels != 3:
+            # Replace first conv layer
+            new_conv_stem = nn.Conv2d(
+                in_channels=input_channels,
+                out_channels=self.feature_extractor.conv_stem.out_channels,
+                kernel_size=self.feature_extractor.conv_stem.kernel_size,
+                stride=self.feature_extractor.conv_stem.stride,
+                padding=self.feature_extractor.conv_stem.padding,
+                bias=self.feature_extractor.conv_stem.bias is not None
+            )
+            with torch.no_grad():
+                new_conv_stem.weight[:,:3] = self.feature_extractor.conv_stem.weight
+                new_conv_stem.weight[:,3] = self.feature_extractor.conv_stem.weight.mean(dim=1)
+        
+            self.feature_extractor.conv_stem = new_conv_stem
 
         self.mlp = MLP(
             in_channels=1280,
@@ -19,6 +35,6 @@ class EfficientNet_B0(nn.Module):
         )
 
     def forward(self, x):
-        x = self.model(x)
+        x = self.feature_extractor(x)
         x = self.mlp(x)
         return x
