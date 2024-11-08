6e825692a561aaa840ff505751d16bc27ff98ce2
Jiaqi-Lv <jiaqi.lv@warwick.ac.uk>
2024-11-08 17:13:36 +0000

    update classification model

--- monkey/model/classification_model/efficientnet_b0.py
+++ monkey/model/classification_model/efficientnet_b0.py
@@ -9,40 +9,35 @@ class EfficientNet_B0(nn.Module):
         self, input_channels=3, num_classes=2, pretrained=True
     ):
         super(EfficientNet_B0, self).__init__()
-        self.feature_extractor = timm.create_model(
+        self.model = timm.create_model(
             "efficientnet_b0",
             pretrained=pretrained,
-            num_classes=0,
+            num_classes=num_classes,
         )
 
         if input_channels != 3:
             # Replace first conv layer
             new_conv_stem = nn.Conv2d(
                 in_channels=input_channels,
-                out_channels=self.feature_extractor.conv_stem.out_channels,
-                kernel_size=self.feature_extractor.conv_stem.kernel_size,
-                stride=self.feature_extractor.conv_stem.stride,
-                padding=self.feature_extractor.conv_stem.padding,
-                bias=self.feature_extractor.conv_stem.bias
+                out_channels=self.model.conv_stem.out_channels,
+                kernel_size=self.model.conv_stem.kernel_size,
+                stride=self.model.conv_stem.stride,
+                padding=self.model.conv_stem.padding,
+                bias=self.model.conv_stem.bias
                 is not None,
             )
             with torch.no_grad():
                 new_conv_stem.weight[
                     :, :3
-                ] = self.feature_extractor.conv_stem.weight
+                ] = self.model.conv_stem.weight
                 new_conv_stem.weight[
                     :, 3
-                ] = self.feature_extractor.conv_stem.weight.mean(
+                ] = self.model.conv_stem.weight.mean(
                     dim=1
                 )
 
-            self.feature_extractor.conv_stem = new_conv_stem
-
-        self.fc = nn.Sequential(
-            nn.Linear(1280, num_classes), nn.ReLU(), nn.Dropout(p=0.3)
-        )
+            self.model.conv_stem = new_conv_stem
 
     def forward(self, x):
-        x = self.feature_extractor(x)
-        x = self.fc(x)
+        x = self.model(x)
         return x
